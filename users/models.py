74d41f2b7e0ff25532b438cfa4ad0bf90179d2b4
FaysalXulhash <faysalahammed80@gmail.com>
2024-09-23 11:57:15 +0600

    first commit

---
+++ users/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    img = models.ImageField(default = 'default.JPG', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile!'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        i = Image.open(self.img.path)
+
+        if i.height > 300 or i.width > 300:
+            output_size = (300, 300)
+            i.thumbnail(output_size)
+            i.save(self.img.path)
+    
