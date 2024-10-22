d992e8e3385cf6fd35132960cb003c3bb586e36e
FaysalXulhash <faysalahammed80@gmail.com>
2024-10-22 07:04:18 +0600

    resolve static files

--- django_project/settings.py
+++ django_project/settings.py
@@ -126,7 +126,10 @@ USE_TZ = True
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = 'static/'
-STATICFILES_DIRS =[BASE_DIR / "static"]
+#STATICFILES_DIRS =[BASE_DIR / "static"]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'media')
+]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
